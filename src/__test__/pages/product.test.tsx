28b3bc05c1ad7ee8046cf84925aca6dc92d567fc
mustofa adny <adniadni04@gmail.com>
2024-07-18 20:18:17 +0700

    learn testing in nextJS

---
+++ src/__test__/pages/product.test.tsx
@@ -0,0 +1,31 @@
+import ProductPage from "@/pages/product";
+import { render, screen } from "@testing-library/react";
+import { useRouter } from "next/router";
+
+jest.mock("next/router", () => {
+  return {
+    useRouter() {
+      return {
+        route: "/product",
+        pathname: "",
+        query: "",
+        asPath: "",
+        push: jest.fn(),
+        events: {
+          on: jest.fn(),
+          off: jest.fn(),
+        },
+        beforePopState: jest.fn(() => null),
+        prefetch: jest.fn(() => null),
+        isReady: true,
+      };
+    },
+  };
+});
+
+describe("Product Page", () => {
+  it("render product page", () => {
+    const page = render(<ProductPage />);
+    expect(page).toMatchSnapshot();
+  });
+});
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function AboutPage() {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1 id="title" data-testid="title">
+        About Page
+      </h1>
     </div>
   );
 }
